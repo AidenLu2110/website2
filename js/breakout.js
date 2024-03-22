896d4170d14538f7838c07bf3d2ef3ce76fc0182
AidenLu2110 <142829225+AidenLu2110@users.noreply.github.com>
2024-03-22 16:32:04 +0000

    Fri, Mar 22, 2024, 9:32 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,6 +1,98 @@
 rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 closeBtn = document.getElementById('close-btn')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+score = 0
+brickRowCount = 9
+brickColumnCount = 5
+
+
+// Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx : 4,
+    dy: -4,
+}
+
+// Create Paddle protperties
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height -20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+}
+
+//Create brick properties
+brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true,
+}
+
+// Create bricks
+brick = []
+for (let i = 0; 1 < brickRowCount; i++) {
+    brick[i] = []
+    for (let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
+        brick[i][j] = {x, y, ...brickInfo}
+    }
+}
+
+// Draw ball on caanvas
+function drawBall() {
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+// Draw ball on caanvas
+function drawPaddle() {
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+    ctx.fill()
+    ctx.closePath()
+}
+
+// Draw score on canvas
+function drawScore() {
+    ctx.font ='20px Arial'
+    ctx.fillText(`Score: ${score}`, canvas.width-100, 30)
+}
+
+//Draw Bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => {
+        colum.forEach(brick => {
+            cctx.beginPath()
+            ctx.rect(brick.x, brick.y, brick.w, brick.h)
+            ctx.fillStyle = '#0095dd'
+            ctx.fill()
+            ctx.closePath()
+        })
+    })
+}
+
+//Draw Everything
+function draw() {
+    drawPaddle()
+    drawBall()
+    drawScore()
+    drawBricks()
+}
 
 // Rules open and close event handelers
 rulesBtn.addEventListener('click', () => {
